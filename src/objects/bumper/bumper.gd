20cf5df9a2402d8402e50e42a3a00efb3cca88d3
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-11 10:04:32 +0100

    Fixed Bumper scale.

--- src/objects/bumper/bumper.gd
+++ src/objects/bumper/bumper.gd
@@ -4,6 +4,10 @@ extends RigidBody2D
 
 @export var max_strength: int = 500
 @export var score: int = 100
+@export var radius: int = 50:
+	set(v):
+		radius = v
+		_update_radius()
 
 @onready var collision_shape = %CollisionShape
 @onready var sprite: Sprite2D = $Sprite2D
@@ -14,10 +18,20 @@ const BUMPER_WAVE_SCENE = preload("res://src/objects/bumper/bumper_wave.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group(&"bumpers")
+	_update_radius()
+
+
+func _update_radius() -> void:
+	if is_node_ready():
+		collision_shape.shape.radius = radius
+		var s = radius / 50.0
+		sprite.scale = Vector2(s, s)
 
 
 func _on_body_entered(body):
 	if body is Ball:
 		# FIXME Enhance bounce
 		#body.linear_velocity = body.linear_velocity.normalized() * 1500
-		add_child(BUMPER_WAVE_SCENE.instantiate())
+		var wave = BUMPER_WAVE_SCENE.instantiate()
+		wave.scale = sprite.scale
+		add_child(wave)
--- src/objects/bumper/bumper_wave.gd
+++ src/objects/bumper/bumper_wave.gd
@@ -5,6 +5,6 @@ extends Sprite2D
 func _ready():
 	var t := create_tween()
 	t.set_ease(Tween.EASE_OUT)
-	t.tween_property(self, "scale", Vector2(3, 3), 0.8)
+	t.tween_property(self, "scale", scale + Vector2(2, 2), 0.8)
 	t.parallel().tween_property(self, "modulate:a", 0.0, 0.8)
 	t.tween_callback(queue_free)
