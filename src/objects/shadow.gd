a2caf24d2f8b0aacbeb5721231db816ff0337a29
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-16 17:17:10 +0100

    Removed scale that was creating issues.

--- src/objects/shadow.gd
+++ src/objects/shadow.gd
@@ -2,7 +2,7 @@ class_name BodySpriteShadow
 extends Sprite2D
 
 @export var parent_sprite: Sprite2D
-@export var shadow_offset: Vector2 = Vector2(0, 10)
+@export var shadow_offset: Vector2 = Vector2(0, 15)
 @export var shadow_color: Color = Color(0, 0, 0, 0.4)
 var _parent_body: Node2D
 
@@ -10,6 +10,7 @@ var _parent_body: Node2D
 func _ready() -> void:
 	z_as_relative = false
 	z_index = -10
+	top_level = true
 	_parent_body = get_parent()
 	if parent_sprite:
 		texture = parent_sprite.texture
@@ -18,7 +19,7 @@ func _ready() -> void:
 		modulate = shadow_color
 		flip_h = parent_sprite.flip_h
 		flip_v = parent_sprite.flip_v
-		scale = parent_sprite.scale + Vector2(0.1, 0.1)
+		scale = parent_sprite.scale
 		if flip_h: shadow_offset.x *= -1
 		if flip_v: shadow_offset.y *= -1
 	set_process(parent_sprite != null)
@@ -26,6 +27,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	var a: float = -_parent_body.rotation
+	rotation = -a
 	global_position = parent_sprite.global_position + shadow_offset
 	frame = parent_sprite.frame
 
