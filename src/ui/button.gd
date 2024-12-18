d49953af8032fbacbe562a7df4fa94cfa500f4c6
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-19 00:09:45 +0100

    Finally got my "super buttons" working! :)

--- src/ui/button.gd
+++ src/ui/button.gd
@@ -1,19 +1,52 @@
+@tool
 extends Button
+
+@export var icon_texture: Texture2D:
+	set(v):
+		icon_texture = v
+		if is_node_ready():
+			$HBoxContainer/Icon.texture = icon_texture
+
+@export var icon_modulate: Color = Color.BLACK:
+	set(v):
+		icon_modulate = v
+		if is_node_ready():
+			$HBoxContainer/Icon.modulate = icon_modulate
+
+@export var separation: int = 20:
+	set(v):
+		separation = v
+		if is_node_ready():
+			$HBoxContainer.add_theme_constant_override(&"separation", v)
+
+@export var label: String:
+	set(v):
+		label = v
+		if is_node_ready():
+			waving_text.text = v
+
+@export var letter_spacing: int = 0:
+	set(v):
+		letter_spacing = v
+		if is_node_ready():
+			waving_text.letter_spacing = v
+
+@onready var waving_text = $HBoxContainer/WavingText
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	icon_texture = icon_texture
+	icon_modulate = icon_modulate
+	letter_spacing = letter_spacing
+	label = label
+	_on_focus_exited()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_mouse_entered():
-	material.set_shader_parameter("hover", true)
-
-
-func _on_mouse_exited():
+func _on_focus_exited():
 	material.set_shader_parameter("hover", false)
+
+
+func _on_focus_entered():
+	material.set_shader_parameter("hover", true)
+	waving_text.waving = true
---
+++ src/ui/waving_text.gd
@@ -0,0 +1,57 @@
+@tool
+class_name WavingText
+extends HBoxContainer
+
+@export var text: String:
+	set(v):
+		text = v
+		if is_node_ready():
+			_update_label()
+
+@export var letter_spacing: int = 0:
+	set(v):
+		letter_spacing = v
+		if is_node_ready():
+			add_theme_constant_override(&"separation", v)
+
+@export var one_shot: bool = false
+@export var waving: bool = true:
+	set(v):
+		waving = v
+		a = 0.0
+		set_process(v)
+
+@export_range(0, 200) var wave_amplitude: int = 5
+@export_range(1, 20) var speed: int = 10
+
+
+func _ready():
+	letter_spacing = letter_spacing
+	text = text
+
+
+func _update_label() -> void:
+	while get_child_count() > 0:
+		remove_child(get_child(0))
+	for l in text:
+		var ln := Label.new()
+		ln.text = l
+		add_child(ln)
+
+
+var last_ts: int = 0
+var ticks: float = 0.0
+var a: float = 0.0
+
+func _process(delta: float) -> void:
+	a = move_toward(a, TAU, delta * speed)
+	if a >= TAU:
+		a = 0.0
+		set_process(not one_shot)
+
+	var count := get_child_count()
+	var interval := PI / (count - 1)
+	var offset := 0.0
+	for lbl in get_children():
+		lbl.position.y = -clamp((sin(a - offset)), 0, 1) * wave_amplitude
+		offset += interval
