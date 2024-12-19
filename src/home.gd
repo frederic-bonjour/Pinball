cce566249f4ab878fe269a04a6a6901db2837e5d
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-19 12:15:04 +0100

    Added small effect on menu, just for fun :)

--- src/home.gd
+++ src/home.gd
@@ -7,3 +7,11 @@ func _on_play_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_quit_button_mouse_entered() -> void:
+	var qb: Button = $VBoxContainer/QuitButton
+	var vt := VanishingTooltip.make_text("Déjà ?", qb.global_position + Vector2(qb.size.x + 90, qb.size.y / 2))
+	vt.add_theme_color_override(&"font_color", Color.WHITE)
+	vt.distance = Vector2(100, 0)
+	add_child(vt)
