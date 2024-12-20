5f662d61b00bba7efbd75c5cb361e8b6ca865758
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-20 18:40:25 +0100

    Removed logo and added possibility to customize the background color.

--- src/objects/board/launcher_plot.gd
+++ src/objects/board/launcher_plot.gd
@@ -1,8 +1,20 @@
+@tool
 class_name LauncherPlot
 extends Node2D
 
+@export var background_color: Color = Color.WHITE:
+	set(v):
+		background_color = v
+		if $Polygon2D:
+			$Polygon2D.color = background_color
+
 @onready var launcher = %Launcher
 @onready var rotate_on_flip = $Launcher/RotateOnFlip
+
+
+func _ready():
+	background_color = background_color
+
 
 func reset_position() -> void:
 	await rotate_on_flip.reset()
