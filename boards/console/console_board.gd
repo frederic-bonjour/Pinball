8ed1d07ba5bc66b8076c05fb492df1ef831b7104
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-20 18:44:22 +0100

    Added board "Console".

---
+++ boards/console/console_board.gd
@@ -0,0 +1,29 @@
+extends PinballBoard
+
+@onready var launcher_plot: LauncherPlot = $LauncherPlot
+
+
+func _board_ready() -> void:
+	_board_load_ball_in_launcher(new_ball())
+
+
+func _board_load_ball_in_launcher(ball: Ball):
+	await launcher_plot.reset_position()
+	launcher_plot.load_ball(ball)
+
+
+func _on_ball_saver_changed(_active: bool):
+	pass
+
+
+func _board_on_letter_group_completed(group: LetterIndicatorGroup, _ball: Ball):
+	# On this board, activate the kickbacks everytime a group is completed.
+	_activate_kickbacks()
+
+
+func _is_board_complete():
+	return all_brick_groups_cleared
+
+
+func _on_cursor_brick_touched(brick: Brick):
+	brick.update_scale(Vector2(-0.2, 0))
