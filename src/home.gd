21f856e3a8d244989a534924f6baa44f8cf3b37a
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-20 18:43:22 +0100

    Added temporary buttons to start the boards.

--- src/home.gd
+++ src/home.gd
@@ -1,8 +1,14 @@
 extends Control
 
-
+func _ready():
+	for b in $PlayBoardButtons.find_children("*", "Button", false):
+		b.connect(&"pressed", _play_board.bind(b.name.substr(5)))
 func _on_play_button_pressed():
 	SceneManager.start_game()
+
+
+func _play_board(board_name: StringName) -> void:
+	SceneManager.start_game(board_name)
 
 
 func _on_quit_button_pressed():
@@ -10,11 +16,13 @@ func _on_quit_button_pressed():
 
 
 func _on_quit_button_mouse_entered() -> void:
+	"""
 	var qb: Button = $VBoxContainer/QuitButton
-	var vt := VanishingTooltip.make_text_anchored("Already leaving?", $VBoxContainer/QuitButton, HORIZONTAL_ALIGNMENT_RIGHT, VERTICAL_ALIGNMENT_CENTER, Vector2(20, 0))
+	var vt := VanishingTooltip.make_text_anchored("Already leaving?", $VBoxContainer/QuitButton, HORIZONTAL_ALIGNMENT_RIGHT, VERTICAL_ALIGNMENT_CENTER, Vector2(0, -20))
 	vt.add_theme_color_override(&"font_color", Color.WHITE)
-	vt.distance = Vector2(100, 0)
+	vt.distance = Vector2(0, -100)
 	add_child(vt)
+	"""
 
 
 func _on_button_mouse_entered():
