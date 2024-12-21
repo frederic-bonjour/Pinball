71b657233d8367a42c2f64696bad7c7ffe2a0375
Frédéric Bonjour <frederic.bonjour@gmail.com>
2024-12-21 16:15:31 +0100

    Added completion check on Console board.

--- boards/console/computer.gd
+++ boards/console/computer.gd
@@ -23,11 +23,15 @@ extends Node2D
 			_update_screen_contents()
 
 
+var is_complete: bool:
+	get:
+		return started and activated and backup_done
+
 func _ready():
 	screen_contents.text = ""
 
 
-func _on_animation_player_animation_finished(anim_name: StringName):
+func _on_animation_player_animation_finished(_anim_name: StringName):
 	_update_screen_contents()
 
 
@@ -37,6 +41,6 @@ func _update_screen_contents() -> void:
 		c.append("System starting...")
 	if activated:
 		c.append("System activated.")
-	if activated:
+	if backup_done:
 		c.append("Backup done.")
 	screen_contents.text = "\n".join(c)
--- boards/console/console_board.gd
+++ boards/console/console_board.gd
@@ -28,12 +28,12 @@ func _board_on_letter_group_completed(group: LetterIndicatorGroup, _ball: Ball):
 
 
 func _is_board_complete():
-	return all_brick_groups_cleared
+	return all_brick_groups_cleared and %Computer.is_complete
 
 
 func _on_cursor_brick_touched(brick: Brick):
 	brick.update_scale(Vector2(-0.2, 0))
 
 
-func _on_power_on_brick_touched(brick):
+func _on_power_on_brick_touched(_brick):
 	%Computer.started = true
